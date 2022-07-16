867bff2db2761a884870fc32b7239aa09531d5d6
cankutay <cankutayucar@hotmail.com>
2022-07-16 13:06:36 +0300

    memeber page and password change page frontend-backend added

--- CankutayUcarIdentity.UI/Controllers/MemberController.cs
+++ CankutayUcarIdentity.UI/Controllers/MemberController.cs
@@ -1,14 +1,94 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using CankutayUcarIdentity.UI.Models;
+using CankutayUcarIdentity.UI.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-
+using Mapster;
 namespace CankutayUcarIdentity.UI.Controllers
 {
     [Authorize]
     public class MemberController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        
+        public MemberController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [Authorize]
         public IActionResult Index()
         {
+
+            AppUser user = _userManager.FindByNameAsync(HttpContext.User.Identity?.Name).Result;
+            UserViewModel model = user.Adapt<UserViewModel>();
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult PasswordChange()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PasswordChange(PassordChangeViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = await _userManager.FindByNameAsync(HttpContext.User.Identity?.Name);
+                if (user != null)
+                {
+                    bool exist = await _userManager.CheckPasswordAsync(user, model.PasswordOld);
+                    if (exist)
+                    {
+                        if (model.PasswordNew == model.PasswordConfirm)
+                        {
+                            IdentityResult result =
+                                await _userManager.ChangePasswordAsync(user, model.PasswordOld, model.PasswordNew);
+                            if (result.Succeeded)
+                            {
+                                await _userManager.UpdateSecurityStampAsync(user);
+                                await _signInManager.SignOutAsync();
+                                await _signInManager.PasswordSignInAsync(user, model.PasswordNew, true, false);
+                                return RedirectToAction("Index", "Member");
+                            }
+                            else
+                            {
+                                foreach (var error in result.Errors)
+                                {
+                                    ModelState.AddModelError("", error.Description);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            ModelState.AddModelError("", "şifreler birbirinden farklı!");
+                        }
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "eski şifreniz yanlış");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Beklenmedik bir hata!");
+                }
+            }
+            else
+            {
+                foreach (var errors in ModelState.Values)
+                {
+                    foreach (var error in errors.Errors)
+                    {
+                        ModelState.AddModelError("", error.ErrorMessage);
+                    }
+                }
+            }
+            return View(model);
         }
     }
 }
---
+++ CankutayUcarIdentity.UI/ViewModels/PassordChangeViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CankutayUcarIdentity.UI.ViewModels
+{
+    public class PassordChangeViewModel
+    {
+        [Display(Name = "Eski şifreniz")]
+        [Required(ErrorMessage = "Eski şifreniz gereklidir.")]
+        [DataType(DataType.Password)]
+        [MinLength(4, ErrorMessage = "En az 4 karakterli olamlıdır")]
+        public string PasswordOld { get; set; }
+
+        [Display(Name = "Yeni şifreniz")]
+        [Required(ErrorMessage = "Yeni şifreniz gereklidir.")]
+        [DataType(DataType.Password)]
+        [MinLength(4, ErrorMessage = "En az 4 karakterli olamlıdır")]
+        public string PasswordNew { get; set; }
+
+        [Display(Name = "Yeni şifreniz tekrar")]
+        [Required(ErrorMessage = "Yeni şifreniz tekrarı gereklidir.")]
+        [DataType(DataType.Password)]
+        [MinLength(4, ErrorMessage = "En az 4 karakterli olamlıdır")]
+        [Compare("PasswordNew",ErrorMessage = "yeni şifre ve yeni şifre tekrarı aynı olmak zorundadır")]
+        public string PasswordConfirm { get; set; }
+    }
+}
