f06004af9df2450c88f7d2025c3017e13da409b1
cankutay <cankutayucar@hotmail.com>
2022-07-31 12:59:53 +0300

    ser roles shor tagg helpers

---
+++ CankutayUcarIdentity.UI/CustomTagHelpers/UserRoleNames.cs
@@ -0,0 +1,33 @@
+﻿using CankutayUcarIdentity.UI.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace CankutayUcarIdentity.UI.CustomTagHelpers
+{
+    [HtmlTargetElement("td", Attributes = "user-roles")]
+    public class UserRoleNames : TagHelper
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public UserRoleNames(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HtmlAttributeName("user-roles")]
+        public string UserId { get; set; }
+
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            AppUser user = await _userManager.FindByIdAsync(UserId);
+            List<string>? userRoles = await _userManager.GetRolesAsync(user) as List<string>;
+            string html = string.Empty;
+            userRoles.ForEach(x =>
+            {
+                html += $"<span class=\"badge rounded-pill bg-success\">{x}</span>";
+            });
+            output.Content.SetHtmlContent(html);
+        }
+    }
+}
